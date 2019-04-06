eb7f67676194b31e6dae75403a0e8b6afbc9efbe
Sies Reynault <reynault.sies8@etu.univ-lorraine.fr>
2019-04-06 16:54:42 +0200

    Commentaires plus réponse question rapport

--- optimisation.py
+++ optimisation.py
@@ -2,21 +2,38 @@
 https://sourcemaking.com/design_patterns/composite/python/1
 """
 
+"""
+Classe modelisant un algo de B&B
+"""
 class BranchAndBound(object):
     """
-    Classe modelisant un algo de B&B
+        Constructeur qui prend un problème en paramètre
     """
-    def __init__(self, pbactuel)
+    def __init__(self, pbactuel):
         super(BranchAndBound, self).__init__()
-        self.pbactuel = pbactuel
+        # Meilleure solution actuelle contenue dans le problème
+        # qui a été évalué avec la troisième heuristique
+        pbactuel = pbactuel
+        self.meilleure = pbactuel
 
+    """
+        Méthode évaluer qui permet d'évaluer le problème en appliquant l'algorithme
+        de branch and bound
+    """
     def evaluer(self):
+        # On vérifie si le problème a une solution
         
-        
+        # Evaluation du problème avec la création de deux première solutions
+        print("oui")
 
+
+"""
+Classe modelisant un probleme de programation lineaire
+"""
 class PL(object):
     """
-    Classe modelisant un probleme de programation lineaire
+        Constructeur qui prend l'ensemble des paramètres et la valeur max correspondant à la contrainte
+        (1000 pour le problème)
     """
     def __init__(self, valMax, *params):
         super(PL, self).__init__()
@@ -25,22 +42,34 @@ class PL(object):
         for param in params:
             self.params.update({param: 0})
 
+    """
+        Méthode qui remet les variables à zero
+    """
     def reset(self):
         for param in self.params:
             self.params[param] = 0
-
+            
+    """
+        Méthode qui permet de récupérer la durée totale
+    """
     def evaluateZ(self):
         result = 0
         for param, coef in self.params.items():
             result = result + (param.duree * coef)
         return result
 
+    """
+        Méthode qui permet de récupérer le coût total
+    """
     def coutZ(self):
         result = 0
         for param, coef in self.params.items():
             result = result + (param.cout * coef)
         return result
 
+    """
+        Méthode qui permet de récupérer l'ordre des variables en fonction de la première heuristique
+    """
     def affiche_heurestique1(self):
         resulttemp = []
         for param, coef in self.params.items():
@@ -50,7 +79,10 @@ class PL(object):
         for p in result:
             string += str(p.nom + " ")
         return string
-
+    
+    """
+        Méthode qui permet de récupérer l'ordre des variables en fonction de la deuxième heuristique
+    """
     def affiche_heurestique2(self):
         resulttemp = []
         for param, coef in self.params.items():
@@ -61,6 +93,9 @@ class PL(object):
             string += str(p.nom + " ")
         return string
 
+    """
+        Méthode qui permet de récupérer l'ordre des variables en fonction de la troisième heuristique
+    """
     def affiche_heurestique3(self):
         resulttemp = []
         for param, coef in self.params.items():
@@ -72,6 +107,9 @@ class PL(object):
             string += str(p.nom + " ")
         return string
 
+    """
+        Méthode d'évaluation du problème linéaire avec la première heuristique
+    """
     def evaluateZ_Heurestique1(self):
         resulttemp = []
         for param, coef in self.params.items():
@@ -89,7 +127,9 @@ class PL(object):
         if (i < len(self.params)):
             self.params[result[i]] = ((self.valMax - self.coutZ()) /
                                       float(result[i].cout))
-
+    """
+        Méthode d'évaluation du problème linéaire avec la deuxième heuristique
+    """
     def evaluateZ_Heurestique2(self):
         resulttemp = []
         for param, coef in self.params.items():
@@ -106,7 +146,9 @@ class PL(object):
         if (i < len(self.params)):
             self.params[result[i]] = ((self.valMax - self.coutZ()) /
                                       float(result[i].cout))
-
+    """
+        Méthode d'évaluation du problème linéaire avec la troisième heuristique
+    """
     def evaluateZ_Heurestique3(self):
         resulttemp = []
         for param, coef in self.params.items():
@@ -124,18 +166,23 @@ class PL(object):
         if (i < len(self.params)):
             self.params[result[i]] = ((self.valMax - self.coutZ()) /
                                       float(result[i].cout))
-
+    """
+        Méthode toString pour afficher les informations du problème linéaire
+    """
     def __str__(self):
-        string = ""
+        string = "Valeurs : \n\n"
         for param, coef in self.params.items():
             string += str(param.nom) + " = " + str(coef) + "\n"
-        string += "Duree de la solution = " + str(self.evaluateZ()) + "\n"
+        string += "\nDuree de la solution = " + str(self.evaluateZ()) + "\n"
         string += "Cout de la solution = " + str(self.coutZ()) + "\n"
         return string
 
+"""
+Classe de representation d'un parametre de programmation lineaire
+"""
 class Param(object):
     """
-    Classe de representation d'un parametre de programmation lineaire
+        Constructeur qui init le nom, la durée et le coût
     """
     def __init__(self, nom, duree, cout):
         super(Param, self).__init__()
@@ -146,7 +193,11 @@ class Param(object):
     def __repr__(self):
         return repr((self.nom, self.duree, self.cout))
 
+"""
+    Méthode main qui va afficher les données voulues
+"""
 def main():
+    # Initialisation du problème linéaire
     p1 = Param("x1", 9, 450)
     p2 = Param("x2", 2, 700)
     p3 = Param("x3", 3, 350)
@@ -154,23 +205,43 @@ def main():
     p5 = Param("x5", 6, 450)
     p6 = Param("x6", 5, 100)
     plineaire = PL(1000, p1, p2, p3, p4, p5, p6)
+    # Affichage des réponses Questions 2 et 3
+    affichageHeuristiques(plineaire)
+    # Affichage du branch and bound
+    applicationBranchAndBound(plineaire)
+    
+    
+
+"""
+    Méthode d'application de branch and bound sur le problème relaxé
+    avec la troisième heuristique
+"""
+def applicationBranchAndBound(plineaire):
+    print("Méthode de branch and bound \n")
+    branch = BranchAndBound(plineaire)
+    branch.evaluer()
+
+"""
+    Méthode d'affichage des résultats du problème relaxé avec les trois heuristiques
+"""
+def affichageHeuristiques(plineaire):
     print("Heuristique 1 \n")
-    print(plineaire.affiche_heurestique1())
+    print("Ordre : " + plineaire.affiche_heurestique1())
     plineaire.evaluateZ_Heurestique1()
     print(plineaire)
     plineaire.reset()
-
+    
     print("Heuristique 2 \n")
-    print(plineaire.affiche_heurestique2())
+    print("Ordre : " + plineaire.affiche_heurestique2())
     plineaire.evaluateZ_Heurestique2()
     print(plineaire)
     plineaire.reset()
 
     print("Heuristique 3 \n")
-    print(plineaire.affiche_heurestique3())
+    print("Ordre : " + plineaire.affiche_heurestique3())
     plineaire.evaluateZ_Heurestique3()
     print(plineaire)
-    plineaire.reset()
 
+    
 if __name__ == '__main__':
     main()
